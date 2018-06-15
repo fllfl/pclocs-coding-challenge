f379aa930cc5115ea2c1d2d79d005649c710e6ff
fllfl <rimu@fisherieslogistics.com>
2018-06-15 20:53:55 +1200

    create api actions and reducer to handle data

---
+++ PCLocsCodingChallenge/API/Constants.js
@@ -0,0 +1,8 @@
+const constants = {
+  restAPI: 'https:/api-public.sandbox.gdax.com/',
+  webSocketFeed: 'wss://ws-feed-public.sandbox.gdax.com/',
+  currencies: 'currencies',
+  products: 'products',
+};
+
+export default constants;
---
+++ PCLocsCodingChallenge/Actions/Types.js
@@ -0,0 +1,7 @@
+const types = {
+  select_product: 'SELECT_PRODUCT',
+  set_products: 'SET_PRODUCTS',
+  update_product: 'UPDATE_PRODUCT',
+};
+
+export default types;
